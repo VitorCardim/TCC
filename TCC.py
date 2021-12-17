93428048db12d6984b4b8659e42fdb8378e66e9a
Vitor Cardim <vcm3@cin.ufpe.br>
2021-12-16 22:06:01 -0300

    new graphs

--- TCC.py
+++ TCC.py
@@ -15,5 +15,22 @@ df['finalgrade'] = pd.to_numeric(df['finalgrade'], errors='coerce')
 df.loc[(df.finalgrade > 0), 'finalgrade'] = round(df.finalgrade)
 df = df.dropna()
 df_gb = df.groupby(['finalgrade']).size()
+
+plt.figure(1)
+df_gb.plot();
+
+plt.figure(2)
+df.boxplot();
+
+
+plt.figure(3)
+df_gb.plot.barh()
+
+plt.figure(4)
 df_gb.plot(kind = 'bar')
+
+plt.figure(5)
 df_gb.plot.pie(figsize=(10,10))
+
+plt.figure(6)
+df_gb.plot.pie(labels=["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10"],colors=["r", "g", "b", "c", "r", "g", "b", "c", "r", "g", "b"],autopct="%.2f",fontsize=20, figsize=(10, 10),);
